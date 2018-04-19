b2220d07bf3fb94fb751f051917e03dd1085ce19
Daniel <daniel@frytg.com>
2018-04-19 19:57:17 +0200

    First version for cloud functions

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Responds to any HTTP request that can provide a "message" field in the body.
+ *
+ * @param {!Object} req Cloud Function request context.
+ * @param {!Object} res Cloud Function response context.
+ */
+
+ var datastore = require('@google-cloud/datastore')();
+
+exports.importer1 = function importer1(req, res) {
+		console.log(req);
+        var key = datastore.key({namespace: "swr2_v1", path: ["media"]});
+        var request = {key: key, data: {
+					pageId: req.body.pageId,
+					pageTitle: req.body.pageTitle,
+					sectionId: req.body.sectionId,
+					sectionHandle: req.body.sectionHandle,
+					url: req.body.url,
+					language: req.body.language,
+					createdAt: Math.floor(new Date() / 1000)
+				}};
+
+        datastore.save(request, function(err) {
+          if (!err) {
+            console.log(key);
+            res.status(200).send('Eintrag in Datastore gespeichert: ' + key.name);
+          } else {
+            console.log(err);
+          }
+        });
+
+};
